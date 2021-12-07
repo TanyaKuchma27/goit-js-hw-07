facee1f7c9c2d93fe8da8981a728d5bcdc24880f
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-12-07 21:05:15 +0200

    task 1

---
+++ js/01-gallery.js
@@ -0,0 +1,51 @@
+import { galleryItems } from './gallery-items.js';
+
+const galleryRef = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryRef.insertAdjacentHTML('beforeend', galleryMarkup);
+galleryRef.addEventListener('click', onGalleryContainerClick);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+    `;
+    })
+    .join('');
+}
+
+function onGalleryContainerClick(evt) {
+    evt.preventDefault();
+
+    const isGalleryItemEl = evt.target.classList.contains('gallery__image');
+
+    if (!isGalleryItemEl) {
+        return;
+    }
+   
+    const instance = basicLightbox.create(`
+    <img src="${evt.target.dataset.source}">
+     `);
+        
+    instance.show(() => {
+        document.addEventListener("keydown", evt => {
+            const ESC_KEY_CODE = 'Escape';
+            const isEscKey = evt.code === ESC_KEY_CODE;
+           
+            if (isEscKey) {
+                instance.close();
+            }
+        });
+    });
+}
