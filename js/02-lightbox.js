b2308fcdc4439c5f84a9a3f3972d74f85c884a7d
Tanya Kuchma <kuchma.tanya@ukr.net>
2021-12-09 10:30:26 +0200

    update task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -9,10 +9,12 @@ function createGalleryMarkup(galleryItems) {
   return galleryItems
     .map(({ preview, original, description }) => {
       return `
-    <a class="gallery__item" href="${original}">
-        <img class="gallery__image" src="${preview}" alt="${description}" />
-    </a>
-    `;
+      <li>
+        <a class="gallery__item" href="${original}">
+          <img class="gallery__image" src="${preview}" alt="${description}" />
+        </a>
+      </li>  
+      `;
     })
     .join('');
 }
@@ -21,32 +23,3 @@ function createGalleryMarkup(galleryItems) {
         captionDelay: 250,
         captionPosition: 'bottom',
     });
-
-
-
-
-//     function onGalleryContainerClick(evt) {
-//     evt.preventDefault();
-
-//     const isGalleryItemEl = evt.target.classList.contains('gallery__image');
-
-//     if (!isGalleryItemEl) {
-//         return;
-//     }
-   
-//     const instance = basicLightbox.create(`
-//     <img src="${evt.target.dataset.source}">
-//      `);
-        
-//     instance.show(() => {
-//         document.addEventListener("keydown", evt => {
-//             const ESC_KEY_CODE = 'Escape';
-//             const isEscKey = evt.code === ESC_KEY_CODE;
-//           console.log(evt.target);
-           
-//             if (isEscKey) {
-//                 instance.close();
-//             }
-//         });
-//     });
-// }
